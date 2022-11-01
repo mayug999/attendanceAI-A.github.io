73113cf9e621fa0f1f8380904489c86cd413af20
mayug999 <85407961+mayug999@users.noreply.github.com>
2022-11-01 18:29:28 +0530

    Add files via upload

---
+++ attendance/attendance.github.io/JS/buttons.js
@@ -0,0 +1,379 @@
+students_list=[
+    {
+        name:"AANCHAL NAYAK",
+        roll:"01",
+        present:0
+    },
+    {
+        name:"ABDUL OMER FAROOQ",
+        roll:"02",
+        present:0
+    },
+    {
+        name:"ALLAMSETTI PRAVEEN MARTIN",
+        roll:"03",
+        present:0
+    },
+    {
+        name:"ALUGANI HARSHITHA",
+        roll:"04",
+        present:0
+    },
+    {
+        name:"ALURU SAI HARSHITHA",
+        roll:"05",
+        present:0
+    },
+    {
+        name:"ANDE SRIKANTH",
+        roll:"06",
+        present:0
+    },
+    {
+        name:"ATTHELLI SAI SRINATH REDDY",
+        roll:"07",
+        present:0
+    },
+    {
+        name:"BACHU THARUN KUMAR",
+        roll:"08",
+        present:0
+    },
+    {
+        name:"BALA SUMAGNA DONDETI",
+        roll:"09",
+        present:0
+    },
+    {
+        name:"BANOTH SAI VARA PRASAD",
+        roll:"10",
+        present:0
+    },
+    {
+        name:"BANOTH SINDHU",
+        roll:"11",
+        present:0
+    },
+    {
+        name:"BARIDU VISHAL",
+        roll:"12",
+        present:0
+    },
+    {
+        name:"BASAVARAJU SREYA",
+        roll:"13",
+        present:0
+    },
+    {
+        name:"BATHULA NITHIN SAI",
+        roll:"14",
+        present:0
+    },
+    {
+        name:"BOMMINENI SAVINAY REDDY",
+        roll:"15",
+        present:0
+    },
+    {
+        name:"BURUGU RAHUL",
+        roll:"16",
+        present:0
+    },
+    {
+        name:"CHARAGONDLA RAHUL",
+        roll:"17",
+        present:0
+    },
+    {
+        name:"CHEPYALA SAI TEJAS",
+        roll:"18",
+        present:0
+    },
+    {
+        name:"CHETELLI VINAY REDDY ",
+        roll:"19",
+        present:0
+    },
+    {
+        name:"GANESH",
+        roll:"20",
+        present:0
+    },
+    {
+        name:"DONTHULA VAISHNAVI",
+        roll:"21",
+        present:0
+    },
+    {
+        name:"ENDLA KARTHIKESHWAR",
+        roll:"22",
+        present:0
+    },
+    {
+        name:" EPPALA RUKSHITHA",
+        roll:"23",
+        present:0
+    },
+    {
+        name:" G SHASHIVARDHAN",
+        roll:"24",
+        present:0
+    },
+    {
+        name:"GADE SRIMANTH",
+        roll:"25",
+        present:0
+    },
+    {
+        name:"GUDESEE MAHESH",
+        roll:"26",
+        present:0
+    },
+    {
+        name:"GUNDUKADI DEEKSHITHA",
+        roll:"27",
+        present:0
+    },
+    {
+        name:"JADA KASHI NATH",
+        roll:"28",
+        present:0
+    },
+    {
+        name:"JAKKULA SUNIL KUMAR",
+        roll:"29",
+        present:0
+    },
+    {
+        name:"KARMAKONDA VAMSHI VARDHAN",
+        roll:"30",
+        present:0
+    },
+    {
+        name:"KOKKONDA PRANAY",
+        roll:"31",
+        present:0
+    },
+    {
+        name:"KOKKULA YASHMI",
+        roll:"32",
+        present:0
+    },
+    {
+        name:"KORBA NESHA",
+        roll:"33",
+        present:0
+    },
+    {
+        name:"KORUKONDA SHIVA NAINA",
+        roll:"34",
+        present:0
+    },
+    {
+        name:" KOTHA DEEPAK REDDY",
+        roll:"35",
+        present:0
+    },
+    {
+        name:"KAPA SUNIL",
+        roll:"36",
+        present:0
+    },
+    {
+        name:"AMGOTH AKASH",
+        roll:"37",
+        present:0
+    },
+    {
+        name:"MAKALA ROSHNI",
+        roll:"38",
+        present:0
+    },
+    {
+        name:"MALOTHU NEHRU NAIK",
+        roll:"39",
+        present:0
+    },
+    {
+        name:"MANCHE SAMIKSHA YADAV",
+        roll:"40",
+        present:0
+    },
+    {
+        name:"MOHAMMED SARFARAZ ALI",
+        roll:"41",
+        present:0
+    },
+    {
+        name:"MOKKA VIJAY KUMAR",
+        roll:"42",
+        present:0
+    },
+    {
+        name:" RAJESH CHANDAR LAL",
+        roll:"43",
+        present:0
+    },
+    {
+        name:"SUDHAKAR CHANDRA LEKHA",
+        roll:"44",
+        present:0
+    },
+    {
+        name:"BABBURI KEERTHANA",
+        roll:"45",
+        present:0
+    },
+    {
+        name:"P VAMSHI KUMAR GOUD",
+        roll:"46",
+        present:0
+    },
+    {
+        name:"PALLE DINESH REDDY",
+        roll:"47",
+        present:0
+    },
+    {
+        name:"PASHAM ARUN",
+        roll:"48",
+        present:0
+    },
+    {
+        name:"K S P AADITYA SRIKAR",
+        roll:"49",
+        present:0
+    },
+    {
+        name:" PREKSHA GUPTA",
+        roll:"50",
+        present:0
+    },
+    {
+        name:"RAPARLA PHALGUN",
+        roll:"51",
+        present:0
+    },
+    {
+        name:"S ADITYA DHANRAJ",
+        roll:"52",
+        present:0
+    },
+    {
+        name:"SANDEPALLY VARSHINI",
+        roll:"53",
+        present:0
+    },
+    {
+        name:"SATTARSHETTY TEJASWI",
+        roll:"54",
+        present:0
+    },
+    {
+        name:"SATTU CHAITHRIKA PATEL",
+        roll:"55",
+        present:0
+    },
+    {
+        name:"SHAIK ASAD",
+        roll:"56",
+        present:0
+    },
+    {
+        name:"SRAVANI MAHINDRA RAJ",
+        roll:"57",
+        present:0
+    },
+    {
+        name:"SWATHI THAKUR",
+        roll:"58",
+        present:0
+    },
+    {
+        name:"YELAMADDI KEERTHI CHOWDARY",
+        roll:"59",
+        present:0
+    },
+    {
+        name:"ZAIB UNNISA NAYEEM",
+        roll:"60",
+        present:0
+    },
+    {
+        name:"B Jyothi Basu",
+        roll:"21_3501",
+        present:0
+    },
+    {
+        name:"B madhuri",
+        roll:"21_3502",
+        present:0
+    },
+    {
+        name:"B Anjali",
+        roll:"21_3503",
+        present:0
+    },
+    {
+        name:"C rajesh",
+        roll:"21_3504",
+        present:0
+    },
+    {
+        name:"E soujanya",
+        roll:"21_3505",
+        present:0
+    },
+    {
+        name:"K Chithra",
+        roll:"21_3506",
+        present:0
+    },
+]
+
+
+for(var i=0;i<67;i=i+2){
+    // console.log(students_list[i]["name"]+" "+students_list[i]["roll"])
+    document.write(`
+    <div class="row">
+    <div class="col-6">
+        <button class="btn btn-danger btn1">
+            <div class="roll">
+                <b>${students_list[i]["roll"]}</b>
+            </div>
+            <div class="name">
+                ${students_list[i]["name"]}
+            </div>
+        </button>
+    </div>
+    <div class="col-6">
+        <button class="btn btn-danger btn1">
+            <div class="roll">
+                <b>${students_list[i+1]["roll"]}</b>
+            </div>
+            <div class="name">
+                ${students_list[i+1]["name"]}
+            </div>
+        </button>
+    </div>
+    
+    
+</div>
+    `)
+}
+
+document.write(`
+<div class="row">
+<div class="col-6">
+    <button class="btn btn-danger btn1">
+        <div class="roll">
+            <b>${students_list[60]["roll"]}</b>
+        </div>
+        <div class="name">
+            ${students_list[60]["name"]}
+        </div>
+    </button>
+</div>
+</div>
+`)
+
